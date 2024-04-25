b1ac88cf7c47d0ed5e122f255fb0c2b962092f1a
sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-04-26 01:13:07 +0530

    shadcn Added

--- frontend/components/inputbox.tsx
+++ frontend/components/inputbox.tsx
@@ -3,14 +3,20 @@ import React from "react";
 interface InputBoxProps {
   heading: string;
   placeholder: string;
+  onchange: (e: React.ChangeEvent<HTMLInputElement>) => void; // Define onchange prop as a function
 }
 
-const InputBox: React.FC<InputBoxProps> = ({ heading, placeholder }) => {
+const InputBox: React.FC<InputBoxProps> = ({
+  heading,
+  placeholder,
+  onchange,
+}) => {
   return (
     <div className="flex flex-col pt-4">
       <label className="text-sm font-semibold mb-1">{heading}</label>
       <input
         className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500"
+        onChange={onchange}
         type="text"
         placeholder={placeholder}
         aria-label={heading}
--- frontend/pages/Signup.tsx
+++ frontend/pages/Signup.tsx
@@ -1,33 +1,92 @@
 import AuthHeading from "../components/AuthHeading";
 import SubHeading from "../components/AuthsubHeading";
-import InputBox from "../components/inputbox";
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar"; 
+import Navbar from "../components/Navbar";
+import InputBox from "../components/inputbox";
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function SignupPage() {
+  const [userName, setUserName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { toast } = useToast();
+  const handleSubmit = () => {
+    console.log(userName);
+    console.log(firstName);
+    console.log(lastName);
+    console.log(password);
+    try {
+      axios.post("http://localhost:3000/api/v1/user/signup", {
+        userName,
+        firstName,
+        lastName,
+        password,
+      });
+      toast({
+        title: "Sign Up Successful",
+        description: "Lets Go",
+      });
+    } catch (e) {
+      console.log("Server Not Responding");
+      toast({
+        title: "Oop's Something Went Wrong",
+        description: "Sign Up Unsuccessful",
+      });
+    }
+  };
+
   return (
     <div className="bg-gray-300 min-h-screen font-roboto">
-      <Navbar /> 
+      <Navbar />
       <div className="flex items-center justify-center pt-16">
         <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
           <div className="text-center">
             <AuthHeading label="Sign Up" />
             <SubHeading label="Enter Your Information To Create an Account" />
           </div>
-          <form className="mt-4">
-            <InputBox heading="Username" placeholder="xxx@gmail.com" />
-            <InputBox heading="Firstname" placeholder="Sanghrakshit" />
-            <InputBox heading="Lastname" placeholder="gautam" />
-            <InputBox heading="Password" placeholder="*******" />
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
+
+          <InputBox
+            onchange={(e) => {
+              setUserName(e.target.value);
+            }}
+            heading="Username"
+            placeholder="xxx@gmail.com"
+          />
+          <InputBox
+            onchange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            heading="Firstname"
+            placeholder="Sanghrakshit"
+          />
+          <InputBox
+            onchange={(e) => {
+              setLastName(e.target.value);
+            }}
+            heading="Lastname"
+            placeholder="gautam"
+          />
+          <InputBox
+            onchange={(e) => {
+              setPassword(e.target.value);
+            }}
+            heading="Password"
+            placeholder="*******"
+          />
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Sign Up
+            </button>
+          </div>
+
           <div className="py-2 text-sm flex justify-center">
             <div>Already have an account?</div>
             <Link
