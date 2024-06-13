23f5ebf5464c02892d7f42d29cc0b9944c2d3d44
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-06-13 21:30:57 +0530

    Balance Type error removed

--- frontend/components/AppNavbars.tsx
+++ frontend/components/AppNavbars.tsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+interface Balance {
+  Balance: number;
+}
+
 export default function AppNavbar() {
   const navigate = useNavigate();
-  const [balance, setBalance] = useState("");
+  const [balance, setBalance] = useState<Balance | null>(null);
   const [loading, setLoading] = useState(true);
   const isLoggedIn = localStorage.getItem("token");
+
   useEffect(() => {
     const fetchBalance = async () => {
       try {
@@ -27,15 +31,17 @@ export default function AppNavbar() {
       }
     };
 
-    fetchBalance();
-  }, []);
+    if (isLoggedIn) {
+      fetchBalance();
+    }
+  }, [isLoggedIn]);
 
   return (
     <div className="flex items-center justify-between bg-gray-900 p-5 pl-10 pr-4">
       <div className="flex items-center">
         <div className="text-xl font-bold text-white">
           <Link to={"/"} className="font-serif font-semibold">
-            MoneyTransferX{" "}
+            MoneyTransferX
           </Link>
         </div>
       </div>
@@ -44,7 +50,7 @@ export default function AppNavbar() {
           <div className="text-white">Loading...</div>
         ) : (
           balance && (
-            <div className="text-xl  font-semibold text-white px-4">
+            <div className="text-xl font-semibold text-white px-4">
               Balance: {balance.Balance}
             </div>
           )
@@ -56,7 +62,7 @@ export default function AppNavbar() {
               navigate("/");
             }}
             type="button"
-            className="bg-red-600 text-white px-5 py-2 rounded-md hover:bg-red-500  focus:outline-none focus:bg-red-500"
+            className="bg-red-600 text-white px-5 py-2 rounded-md hover:bg-red-500 focus:outline-none focus:bg-red-500"
           >
             Logout
           </button>
