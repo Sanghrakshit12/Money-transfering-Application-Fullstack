508bf2e41e306d98c701dfb09b18b613e355f581
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-06-14 15:24:00 +0530

    backend Dockerized

---
+++ backend/script.sh
@@ -0,0 +1,7 @@
+docker run -v mtx_vol:/data/db --name mtx_db --network mtx_ntw -p 27017:27017 mongo
+docker run -p 3000:3000 \
+  -e Mongo_URL="mongodb://mtx_db:27017/MoneyTransferX" \
+  -e JWT_Secret="NITJ25" \
+  --name mtx_backend \
+  --network mtx_ntw \
+  moneytransferxbackend:v1
